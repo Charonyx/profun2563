51466590dd44ae27bc2051608f3eb9ae063357f2
CHURAIRAT DANGPHUNGPAIBOON <63010159@KMITL.AC.TH>
2020-09-05 22:03:12 +0700

    add note

--- count _never/count _never.cpp
+++ count _never/count _never.cpp
@@ -1,17 +1,20 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
-int main() {
-	char text[10000];
+int main() 
+{
+	char text[1000];
 	int i = 0, count = 0;
 	scanf("%[^\n]", text);
-	while (text[i] != '\0') {
+	while (text[i] != '\0') 
+	{
 		if (text[i] >= 'A' && text[i] <= 'Z')
 			text[i] += 32;
-		if (text[i - 4] == 'n' && text[i - 3] == 'e' && text[i - 2] == 'v' && text[i - 1] == 'e' && text[i] == 'r') {
+		if (text[i - 4] == 'n' && text[i - 3] == 'e' && text[i - 2] == 'v' && text[i - 1] == 'e' && text[i] == 'r') 
+		{
 			count++;
 		}
 		i++;
 	}
-	printf("%d", count);
+	printf("%d ", count);
 	return 0;
 }
