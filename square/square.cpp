384f7bf83879ee9487f3d3c36bb348cf3b7b63fd
CHURAIRAT DANGPHUNGPAIBOON <63010159@KMITL.AC.TH>
2020-09-05 23:33:13 +0700

    edit ui

--- square/square.cpp
+++ square/square.cpp
@@ -4,8 +4,8 @@ void peri(float, float);
 void area(float, float);
 int main()
 {
-	int a, b;
-	printf("Enter the width of a rectangle : ");
+	float a, b;
+	printf("Enter the width of a rectangle \t: ");
 	scanf("%f", &a);
 	printf("Enter the height of a rectangle : ");
 	scanf("%f", &b);
@@ -16,15 +16,15 @@ int main()
 	}
 	else
 	{
-		printf("Can't make a rectangle.\nEnter a positive number, please.");
+		printf("\nCan't make a rectangle.\nEnter a positive number, please.");
 	}
 	return 0;
 }
 void peri(float a, float b)
 {
-	printf("Perimeter \t\t : %.2f\n", 2 * (a + b));
+	printf("Perimeter \t\t\t: %.2f\n", 2 * (a + b));
 }
 void area(float a, float b)
 {
-	printf("Area \t\t : %.2f", a * b);
+	printf("Area \t\t\t\t: %.2f", a * b);
 }
