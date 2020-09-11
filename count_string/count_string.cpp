8c2c3ba4c75e78348ba48379751841922cb36de3
CHURAIRAT DANGPHUNGPAIBOON <63010159@KMITL.AC.TH>
2020-09-11 21:08:32 +0700

    edit ui

--- count_string/count_string.cpp
+++ count_string/count_string.cpp
@@ -1,31 +1,23 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 int main() {
-	char text[10000];
-	int i = 0, al = 0, vo = 0, w = 0, num = 0;
+	char text[1000];
+	int i = 0, al = 0, vo = 0, num = 0;
 	scanf("%[^\n]", text);
 	while (text[i] != '\0') {
-		if (text[i] == 65 || text[i] == 69 || text[i] == 73 || text[i] == 79 || text[i] == 85 || text[i] == 97 || text[i] == 101 || text[i] == 105 || text[i] == 111 || text[i] == 117) {
+		if (text[i] == 'A' || text[i] == 'E' || text[i] == 'I' || text[i] == 'O' || text[i] == 'U' || text[i] == 'a' || text[i] == 'e' || text[i] == 'i' || text[i] == 'o' || text[i] == 'u') {
 			vo++;
 		}
-		else if ((text[i] >= 65 && text[i] <= 90) || (text[i] >= 97 && text[i] <= 122)) {
+		else if ((text[i] >= 'A' && text[i] <= 'Z') || (text[i] >= 'a' && text[i] <= 'z')) {
 			al++;
 		}
 		else if (text[i] >= 48 && text[i] <= 57) {
 			num++;
 		}
-		//		else if(text[i] == ' ' ){
-		//			w++;
-		//		}
 
 		i++;
 	}
-
-	//	if(al != 0 && vo != 0 && num != 0)
-	//		w++;
-	//	else if(al == 0 && vo == 0 && num == 0)
-	//		w = 0;
-	printf("Alphabet = %d\nVowel =  %d\nNumber = %d"/*\nWord = %d "*/, al, vo, num/*,w*/);
+	printf("Alphabet = %d\nVowel =  %d\nNumber = %d", al, vo, num);
 
 	return  0;
 }
---
+++ triangleArea/triangleArea.cpp
@@ -0,0 +1,43 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<math.h>
+float s = 0, area = 0;
+void triArea(int side[4]);
+int main()
+{
+	int* ptr;
+	int  side[4] = { 0 },i = 1, error = 0;
+	while (i != 4)
+	{
+		printf("Enter the side (%d) length of a triangle : )", i);
+		scanf("%d", side[i]);
+		if (side[i] <= 0)
+		{
+			error = 1;
+			break;
+		}
+		s += side[i];
+		printf("%f", s);
+		++i;
+	}
+	if (error == 0)
+	{
+		triArea(side);
+	}
+	else 
+	{ 
+		printf("Enter a positive integer, please!"); 
+	}
+	return 0;
+}
+void triArea(int side[4])
+{
+	s /= 2;
+	int i = 1;
+	while (i != 4)
+	{
+		area *= (s - side[i]);
+		++i;
+	}
+	printf("Area : %.2f", sqrt(area));
+}
