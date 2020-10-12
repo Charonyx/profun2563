e4bf8440e2e151ca3d531cae4747a3f47b84eed7
CHURAIRAT DANGPHUNGPAIBOON <63010159@KMITL.AC.TH>
2020-10-12 21:22:18 +0700

    add proj juice

---
+++ exchange-free-juice/exchange-free-juice.cpp
@@ -0,0 +1,40 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int n, b, c, B, x; //bottle & bottle_cap & new_bottle
+void bottle();
+int main()
+{
+	printf("Enter money \t\t: ");
+	scanf("%d", &n);
+	if (n >= 20 && n <= 50000)
+	{
+		bottle();
+		printf("Bottle(s) of juice \t: %d", B);
+	}
+	else
+	{
+		printf("Error!");
+	}
+	return 0;
+}
+void bottle()
+{
+	n /= 20;
+
+	b = n;
+	c = b;
+	B = b;
+
+	while (c >= 4 || b >= 2)
+	{
+		x = b / 2;
+		B += x;
+		b += x - (2 * x);
+		c += x;
+
+		x = int(c / 4);
+		B += x;
+		c += x - (4 * x);
+		b += x;
+	}
+}
